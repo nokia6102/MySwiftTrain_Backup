07a2afafdf94cbe440d81da0e7e72d96102d3f16
MaxCheng <nokia6102@gmail.com>
2017-08-14 08:37:50 +0800

    UP tableview controller

---
+++ xcodefile_bill/Launch/Launch/MainViewController.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+
+class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath)
+      return cell
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
