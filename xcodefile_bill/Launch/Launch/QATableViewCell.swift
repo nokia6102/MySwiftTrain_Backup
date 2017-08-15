8e479da96951f376f676c15635f7260dc6f7dd25
MaxCheng <nokia6102@gmail.com>
2017-08-15 14:17:12 +0800

    介面串好了

---
+++ xcodefile_bill/Launch/Launch/QATableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MainTableViewCell.swift
+//  Launch
+//
+//  Created by MaxCheng on 2017/8/11.
+//  Copyright © 2017年 com.USSwiftCoda. All rights reserved.
+//
+
+import UIKit
+
+class QATableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblNo: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+ 
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
---
+++ xcodefile_bill/Launch/Launch/QAViewController.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+
+class QAViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "QACell", for: indexPath)
+      return cell
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
