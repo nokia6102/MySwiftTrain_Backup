07e3b3d33093712bf4faafa0b6dd738a0de38f31
MaxCheng <nokia6102@gmail.com>
2017-08-15 15:43:02 +0800

    第3頁 pickview

---
+++ xcodefile_bill/Launch/TQViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TQViewController.swift
+//  Launch
+//
+//  Created by MaxCheng on 2017/8/15.
+//  Copyright © 2017年 com.USSwiftCoda. All rights reserved.
+//
+
+import UIKit
+
+class TQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
